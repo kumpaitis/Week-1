57b6e64b2d165a9648f011d75597fd4cef4b5b9b
kumpaitis <tumenas.s@gmail.com>
2014-06-24 02:53:48 +0300

    Update palindrome.rb

--- palindrome.rb
+++ palindrome.rb
@@ -1,7 +1,6 @@
 def palindrome?(sentence)
   flipped_sentence = sentence.downcase.gsub(/\s+/, "").reverse
   sentence = sentence.downcase.gsub(/\s+/, "")
-  puts flipped_sentence
   return true if sentence == flipped_sentence
   return false
 end
